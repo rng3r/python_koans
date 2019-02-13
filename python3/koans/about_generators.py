0ce7ccaf8c40d51043b807c2b4e1ac444470ff99
Anatoly Semenov <anatoly.m.semenov@gmail.com>
2019-02-13 10:46:31 +0300

    fix test_generators_are_different_to_list_comprehensions

--- python3/koans/about_generators.py
+++ python3/koans/about_generators.py
@@ -28,9 +28,9 @@ class AboutGenerators(Koan):
         self.assertEqual(2, num_list[0])
 
         # A generator has to be iterated through.
-        with self.assertRaises(___): num = num_generator[0]
+        with self.assertRaises(TypeError): num = num_generator[0]
 
-        self.assertEqual(__, list(num_generator)[0])
+        self.assertEqual(2, list(num_generator)[0])
 
         # Both list comprehensions and generators can be iterated though. However, a generator
         # function is only called on the first iteration. The values are generated on the fly
