52890d9f31e63eae7460c4a7f230b14a851f1154
Anatoly Semenov <anatoly.m.semenov@gmail.com>
2019-02-14 16:52:06 +0300

    fix some coroutines generators

--- python3/koans/about_generators.py
+++ python3/koans/about_generators.py
@@ -106,7 +106,7 @@ class AboutGenerators(Koan):
         #       section of http://www.python.org/dev/peps/pep-0342/
         next(generator)
 
-        self.assertEqual(__, generator.send(1 + 2))
+        self.assertEqual(3, generator.send(1 + 2))
 
     def test_before_sending_a_value_to_a_generator_next_must_be_called(self):
         generator = self.coroutine()
@@ -114,7 +114,7 @@ class AboutGenerators(Koan):
         try:
             generator.send(1 + 2)
         except TypeError as ex:
-            self.assertRegex(ex.args[0], __)
+            self.assertRegex(ex.args[0], "can't send non-None value to a just-started generator")
 
     # ------------------------------------------------------------------
 
@@ -132,11 +132,11 @@ class AboutGenerators(Koan):
 
         generator2 = self.yield_tester()
         next(generator2)
-        self.assertEqual(__, next(generator2))
+        self.assertEqual('no value', next(generator2))
 
     def test_send_none_is_equivalent_to_next(self):
         generator = self.yield_tester()
 
         next(generator)
         # 'next(generator)' is exactly equivalent to 'generator.send(None)'
-        self.assertEqual(__, generator.send(None))
+        self.assertEqual('no value', generator.send(None))
